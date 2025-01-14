2f38ddcdf554dc8fa2756d060bd81e6acd475440
rmironko <01187254@pw.edu.pl>
2025-01-14 10:25:27 +0100

    Trying to fix this weird error

--- src/main/java/com/pap/shopping/list/PapShoppingList/controller/AuthController.java
+++ src/main/java/com/pap/shopping/list/PapShoppingList/controller/AuthController.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:5173")
 public class AuthController {
 
     private final DbService dbService;
